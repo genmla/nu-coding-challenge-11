cdd2b81ef926a21415c5c02a538e65efc382b2ca
genmla <gmmladenka@gmail.com>
2022-12-11 15:36:59 -0600

    add post fetch that pushes saved notes to db.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const db = require('./db/db.json')
 const PORT = 3001;
@@ -23,7 +24,56 @@ app.get('/notes', (req, res) =>
 
 app.get('/api/notes', (req, res) => res.json(db));
 
-app.post('/api/notes', (req, res) => res.json(db));
+//post notes
+app.post('/api/notes', (req, res) => {
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+      };
+  
+// Obtain existing notes
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      // Add a new review
+      parsedNotes.push(newNote);
+
+      // Write updated reviews back to the file
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
+    }
+  });
+
+  const response = {
+    status: 'success',
+    body: newReview,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+} else {
+  res.status(500).json('Error in posting review');
+    }
+  }
+
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
