26710e1132df23d8d27d4b26bc52421c26b76bb5
genmla <gmmladenka@gmail.com>
2022-12-11 14:46:13 -0600

    add get fetch to display notes

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const db = require('./db/db.json')
 const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//route for homepage (index.html)
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//route for notes page (notes.html)
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => res.json(db));
+
+app.post('/api/notes', (req, res) => res.json(db));
+
 app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
